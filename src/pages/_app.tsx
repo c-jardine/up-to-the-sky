d40d9105c7d7d1ec12595b638c260c9b86a1a7f3
Chris Jardine <chris@keplux.com>
2023-02-28 12:48:26 -0500

    Hide loading indicator in studio.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,7 +42,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       />
       {!router.asPath.startsWith('/studio') && <Navbar />}
       <Box position="relative" className={poppins.className}>
-        {isLoading && (
+        {!router.asPath.startsWith('/studio') && isLoading && (
           <Box
             position="fixed"
             top={0}
